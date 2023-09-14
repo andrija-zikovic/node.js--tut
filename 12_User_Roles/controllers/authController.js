7060607af4042995a98784b8b3663c6d048eb15c
Andrija Žiković <126960888+andrija-zikovic@users.noreply.github.com>
2023-09-14 13:16:16 +0000

    new file:   12_User_Roles/config/roles_list.js 	modified:   12_User_Roles/controllers/authController.js 	modified:   12_User_Roles/controllers/refreshTokenController.js 	modified:   12_User_Roles/controllers/registerController.js 	modified:   12_User_Roles/logs/errLog.txt 	modified:   12_User_Roles/logs/reqLog.txt 	modified:   12_User_Roles/middleware/verifyJWT.js 	new file:   12_User_Roles/middleware/verifyRoles.js 	modified:   12_User_Roles/model/users.json 	modified:   12_User_Roles/routes/api/employees.js

--- 12_User_Roles/controllers/authController.js
+++ 12_User_Roles/controllers/authController.js
@@ -4,7 +4,7 @@ const usersDB = {
 }
 const bcrypt = require('bcrypt');
 
-const JWT = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const fsPromises = require('fs').promises;
 const path = require('path');
@@ -13,17 +13,23 @@ const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
     const foundUser = usersDB.users.find(person => person.username === user);
-    if (!foundUser) return res.sendStatus(401); //Unauthorized
-    // evaluate password
+    if (!foundUser) return res.sendStatus(401); //Unauthorized 
+    // evaluate password 
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
+        const roles = Object.values(foundUser.roles);
         // create JWTs
-        const accessToken = JWT.sign(
-            { "username": foundUser.username },
+        const accessToken = jwt.sign(
+            {
+                "UserInfo": {
+                    "username": foundUser.username,
+                    "roles": roles
+                }
+            },
             process.env.ACCESS_TOKEN_SECRET,
             { expiresIn: '30s' }
         );
-        const refreshToken = JWT.sign(
+        const refreshToken = jwt.sign(
             { "username": foundUser.username },
             process.env.REFRESH_TOKEN_SECRET,
             { expiresIn: '1d' }
@@ -39,7 +45,7 @@ const handleLogin = async (req, res) => {
         res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
         res.json({ accessToken });
     } else {
-        res.sendStatus(401)
+        res.sendStatus(401);
     }
 }
 
